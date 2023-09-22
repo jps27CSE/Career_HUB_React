377c6c2521b5a20e0672e8c505178c1c1d8d832d
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-09-23 01:18:52 +0600

    added search function in featured jobs

--- src/components/Home/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/Home/FeaturedJobs/FeaturedJobs.jsx
@@ -2,12 +2,29 @@ import { useEffect, useState } from "react";
 import FeaturedJob from "./FeaturedJob.jsx/FeaturedJob";
 
 const FeaturedJobs = () => {
+  const [originalFeaturedJobs, setOriginalFeaturedJobs] = useState([]);
   const [featuredJobs, setFeaturedJobs] = useState([]);
   const [lengthItems, setLengthItems] = useState(4);
+  const [searchField, setSearchField] = useState("");
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchField.length === 0) {
+      setFeaturedJobs(originalFeaturedJobs);
+    } else {
+      const filterJobs = featuredJobs.filter((job) =>
+        job.job_title.toLowerCase().includes(searchField.toLowerCase())
+      );
+      setFeaturedJobs(filterJobs);
+    }
+  };
+
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
-      .then((data) => setFeaturedJobs(data));
+      .then((data) => {
+        setOriginalFeaturedJobs(data), setFeaturedJobs(data);
+      });
   }, [lengthItems]);
   return (
     <div>
@@ -16,6 +33,19 @@ const FeaturedJobs = () => {
         Explore thousands of job opportunities with all the information you
         need. Its your future
       </p>
+
+      <form
+        onSubmit={handleSearchSubmit}
+        className="flex justify-center mt-2 mb-2"
+      >
+        <input
+          onChange={(e) => setSearchField(e.target.value)}
+          className="border-2 p-2 "
+          type="text"
+          placeholder="Search"
+        />
+        <input type="submit" className="btn btn-primary" />
+      </form>
 
       <div className="grid grid-cols-2 mt-5 mb-5 gap-2">
         {featuredJobs.slice(0, lengthItems).map((job) => (
